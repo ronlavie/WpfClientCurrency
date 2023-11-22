31435800b2f7536711c702087115f0e8cd15457d
ronlavie <ronlavie200@gmail.com>
2023-11-22 15:21:42 +0200

    Add project files.

---
+++ WpfClientCurrency/ImageConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfClientCurrency
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double chage = double.Parse(value.ToString());
+                if (chage < 0)
+                    return new Uri("pack://application:,,,/Images/down.png");
+                else if(chage > 0)
+                    return new Uri("pack://application:,,,/Images/up.png");
+                return new Uri("pack://application:,,,/Images/no-change.png");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfClientCurrency/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfClientCurrency.CurrencyService;
+
+
+namespace WpfClientCurrency
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CurrencyServiceClient currencyService = new CurrencyServiceClient();
+        private CurrencyList currencyList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+             
+
+            currencyList = currencyService.GetAllCurrencies();
+            lvCurrencies.ItemsSource = currencyList;
+            cmbSource.ItemsSource = currencyList;
+            cmbSource.DisplayMemberPath = "key";
+            cmbTarget.ItemsSource = currencyList;
+            cmbTarget.DisplayMemberPath= "key";
+        }
+
+        private void tbAmount_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Char.IsDigit(e.Text[0]);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Currency source = cmbSource.SelectedItem as Currency;
+            Currency target = cmbTarget.SelectedItem as Currency;
+            double amount = int.Parse(tbAmount.Text);
+            double result= currencyService.Convert(source, target, amount);
+            tbResult.Text = $"{amount} {source.key} is {result} is {target.key}";
+
+        }
+    }
+}
